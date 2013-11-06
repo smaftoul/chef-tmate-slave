8fa9fa3e890735715e81229f719d9e4cd1c747d4
smaftoul <samuel.maftoul@gmail.com>
2013-11-06 15:56:01 +0100

    simplify script "compile_tmate-slave_part1"
    Manage status with `set -e`

--- recipes/tmate-slave_build.rb
+++ recipes/tmate-slave_build.rb
@@ -1,4 +1,3 @@
-
 git "/tmp/tmate-slave" do
   repository "https://github.com/nviennot/tmate-slave.git"
   action :sync
@@ -11,13 +10,12 @@ script "compile_tmate-slave_part1" do
       user "root"
       cwd "/tmp/tmate-slave"
       code <<-EOH
-        STATUS=0
-        bash create_keys.sh > /root/tmate-slave-footprints.txt || STATUS=1
+        set -e
+        bash create_keys.sh > /root/tmate-slave-footprints.txt
         cp -r /tmp/tmate-slave/keys /root/keys/
         cd /tmp/tmate-slave
-        ./autogen.sh || STATUS=1
-        ./configure || STATUS=1
-        make || STATUS=1
-        exit $STATUS
+        ./autogen.sh
+        ./configure
+        make
       EOH
 end
